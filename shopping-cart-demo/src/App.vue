37a167e45a24ea1d0a3ea5d892d734c43c59dd91
Harrylj <18200166593@163.com>
2019-07-23 23:28:57 +0800

    2019-07-23 23:28

---
+++ shopping-cart-demo/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+      <h1>{{title}}</h1>
+      <hr>
+    <div>
+      <h2>添加课程</h2>
+      <div>
+        <label>课程名称:</label>
+        <input type="text" v-model="courseInfo.name">
+      </div>
+      <div>
+        <label>课程价格:</label>
+        <input type="text" v-model="courseInfo.price">
+      </div>
+      <div><button @click="addCourseToList">添加课程到列表</button></div>
+    </div>
+    <div>
+      <h2>课程列表</h2>
+      <table>
+        <tr>
+          <th>课程名称</th>
+          <th>课程价格</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(item,index) in courseList"  :key="item.id">
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.id}}</td>
+          <td><button @click="addCourseToCart(index)">添加到购物车</button></td>
+        </tr>
+      </table>
+    </div>
+    <cart :courseCart="courseCart"></cart>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Cart from  './components/Cart.vue'
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    Cart
+  },
+  methods:{
+    addCourseToList(){
+      this.courseList.push(this.courseInfo)
+    },
+    addCourseToCart(index){
+      this.courseCart.push(this.courseList[index])
+      console.log(this.courseCart)
+      //alert('添加购物车')
+    }
+  },
+  data : () => {
+    return {
+      title: '55555',
+      courseCart:[
+        {
+          name: '1',
+          price: '22',
+        }
+      ],
+      courseInfo:[
+        {
+          name: '',
+          price: ''
+        }
+      ],
+      courseList:[
+        {
+          id:'0',
+          name: 'web全栈工程师',
+          price: '2000'
+        },
+        {
+          id:'1',
+          name: 'JAVA工程师',
+          price: '3000'
+        }
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ shopping-cart-demo/src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="Cart">
+        <h2>我是购物车</h2>
+        <table>
+            <tr>
+                <td>勾选</td>
+                <td>课程名称</td>
+                <td>课程价格</td>
+                <td>数量</td>
+                <td>价格</td>
+            </tr>
+            <tr v-for="item in courseCart" :key="item.id">
+                <td></td>
+                <td>{{item.name}}</td>
+                <td>{{item.price}}</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Cart",
+        props: ['courseCart']
+    }
+</script>
+
+<style scoped>
+
+</style>
